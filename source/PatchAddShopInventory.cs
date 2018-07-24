ab80939080da2503d37654af40b0683c63d333b4
Matthew Spencer <m22spencer@gmail.com>
2018-07-24 09:42:44 -0500

    Greatly increase performance of Buy/Sell #1

--- source/PatchAddShopInventory.cs
+++ source/PatchAddShopInventory.cs
@@ -2,31 +2,51 @@ using HBS.Logging;
 using Harmony;
 using BattleTech;
 using BattleTech.UI;
-
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace BattletechPerformanceFix
 {
-
-	// Block any sorts until the listview is completely built, and then sort once.
 	[HarmonyPatch(typeof(SG_Shop_Screen), "AddShopInventory")]
     public static class Patch_SG_Shop_Screen_AddShopInventory_OnlySortAtEnd {
-    	public static void Prefix() {
-    		Patch_HBSLoopScrollRect_OptionalSort.WantSort = false;
-    	}
-
     	public static void Postfix(SG_Shop_Screen __instance) {
-    		Patch_HBSLoopScrollRect_OptionalSort.WantSort = true;
-    		Traverse.Create(__instance).Field("ListView")
-    		                           .Method("Sort")
-    		                           .GetValue();
-    	}
+            var _this = Traverse.Create(__instance);
+            var lv = _this.Field("inventoryWidget").Field("ListView");
+
+            //These don't actually seem to be needed, but keeping them just in case.
+            lv.Method("Sort").GetValue();
+            lv.Method("Refresh").GetValue();
+        }
     }
 
-    [HarmonyPatch(typeof(HBSLoopScrollRect<InventoryItemElement, ListElementController_BASE>), "Sort")]
-    public static class Patch_HBSLoopScrollRect_OptionalSort {
-    	public static bool WantSort = false;
-    	public static bool Prefix() {
-    		return WantSort;
-    	}
+    // Perform only the necessary data changes, ignore all UI fixup.
+    [HarmonyPatch(typeof(MechLabInventoryWidget_ListView), "AddItemToInventory")]
+    public static class Patch_AddItemToInventory {
+        public static bool Prefix(MechLabInventoryWidget_ListView __instance, ListElementController_BASE ItemData) {
+            var _this = __instance;
+            var _items = (List<ListElementController_BASE>)Traverse.Create(__instance).Field("ListView").Property("Items").GetValue();
+            ListElementController_BASE listElementController_BASE = null;
+            foreach (ListElementController_BASE listElementController_BASE2 in _this.inventoryData)
+            {
+                if (listElementController_BASE2.GetItemType() == ItemData.GetItemType() && listElementController_BASE2.IsDuplicateContent(ItemData) && _this.ParentDropTarget != null && _this.StackQuantities)
+                {
+                    listElementController_BASE = listElementController_BASE2;
+                    break;
+                }
+            }
+
+            if (listElementController_BASE != null)
+            {
+                listElementController_BASE.ModifyQuantity(1);
+            }
+            else
+            {
+                // OnItemAdded logic does not seem to be needed?
+                // HBSLoopScroll 219-228
+                _items.Add(ItemData);
+            }
+            return false;
+        }
     }
 }
