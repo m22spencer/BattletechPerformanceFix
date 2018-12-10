f4b7f9220f6357af62bd7070e7556510506f7bef
Matthew Spencer <m22spencer@gmail.com>
2018-12-09 21:08:55 -0600

    Allow successful reload and post misison Leopard loading

--- source/ParallelizeLoad.cs
+++ source/ParallelizeLoad.cs
@@ -11,6 +11,7 @@ using UnityEngine.SceneManagement;
 using BattleTech;
 using BattleTech.UI;
 using BattleTech.Save;
+using System.Diagnostics;
 using static BattletechPerformanceFix.Extensions;
 
 namespace BattletechPerformanceFix
@@ -30,6 +31,19 @@ namespace BattletechPerformanceFix
             Main.harmony.Patch(AccessTools.Method(typeof(SimGameUXCreator), nameof(Start)), new HarmonyMethod(AccessTools.Method(self, nameof(Start))));
             Main.harmony.Patch(AccessTools.Method(typeof(SimGameUXCreator), nameof(OnSimGameInitializeComplete)), new HarmonyMethod(AccessTools.Method(self, nameof(OnSimGameInitializeComplete))));
 
+            Main.harmony.Patch(AccessTools.Method(typeof(BattleTech.LevelLoader), nameof(LoadScene)), new HarmonyMethod(AccessTools.Method(self, nameof(LoadScene))));
+            Main.harmony.Patch(AccessTools.Method(typeof(BattleTech.LevelLoader), "Start"), new HarmonyMethod(AccessTools.Method(self, nameof(LevelLoader_Start))));
+
+        }
+
+        public static void LoadScene(string scene) {
+            var currentScenes = string.Join(" ", SceneManager.GetAllScenes().Select(s => s.name).ToArray());
+            Log($"LL.LoadScene {scene} :currentScenes {currentScenes} from {new StackTrace().ToString()}");
+        }
+
+        public static void LevelLoader_Start() {
+            var currentScenes = string.Join(" ", SceneManager.GetAllScenes().Select(s => s.name).ToArray());
+            Log("LL start triggered :currentScenes {currentScenes}");
         }
 
         public static void SimGameUXCreatorLoaded() {
@@ -51,6 +65,7 @@ namespace BattletechPerformanceFix
 
         public static SimGameUXCreator uxc;
         public static bool Awake(SimGameUXCreator __instance) {
+            if (Scene == null) return true; //Not our scene
             if (uxc == null) {
                 Log("Ignoring SimGameUXCreator.Awake");
                 uxc = __instance;
@@ -65,6 +80,7 @@ namespace BattletechPerformanceFix
         // This is an enumerator, this wont work.
         // lets just grab the IEnumerator from InitializeUX, force it to fully exhaust and then run the Start() body ourselves.
         public static bool Start(SimGameUXCreator __instance) {
+            if (Scene == null) return true; //Not our scene
             if (uxcs == null) {
                 Log("Ignoring SimGameUXCreator.Start");
                 uxcs = __instance;
@@ -77,22 +93,41 @@ namespace BattletechPerformanceFix
 
         public static AsyncOperation Scene;
         public static bool _OnBeginAttachUX(SimGameState __instance) {
+            if (Scene == null) return true;  //Not our scene
             Log("Attach UX and do *not* load scene");
             __instance.DataManager.Clear(false, false, true, true, false);
             ActiveOrDefaultSettings.CloudSettings.customUnitsAndLances.UnMountMemoryStore(__instance.DataManager);
-            if (!Scene.isDone) LogError("Scene is *not ready*");
-            else { Log("Scene is loaded and done uxc? {0}", uxc != null);
-                   SceneManager.SetActiveScene(SceneManager.GetSceneByName("SimGame"));
-                   SceneManager.GetAllScenes().ToList().ForEach(scn => Log($"Scene: {scn.name}"));
-                   new Traverse(uxc).Method("Awake").GetValue(); }
-                   //new Traverse(uxc).Method("Start").GetValue(); }
+
+            CoroutineInvoker.InvokeCoroutine(WaitScene(Scene), () => {
+                    if (!Scene.isDone) LogError("Scene is *not ready*");
+                    else { Log("Scene is loaded and done uxc? {0}", uxc != null);
+                           SceneManager.SetActiveScene(SceneManager.GetSceneByName("SimGame"));
+                           SceneManager.GetAllScenes().ToList().ForEach(scn => Log($"Scene: {scn.name}"));
+                           new Traverse(uxc).Method("Awake").GetValue();
+                           Scene = null; }
+                });
             return false;
         }
 
         public static void _OnBeginDefsLoad() {
-            Log("Load defs in parallel to scene load");
+            uxcs = uxc = null;
+            var currentScenes = string.Join(" ", SceneManager.GetAllScenes().Select(s => s.name).ToArray());
+            Log($"Load defs in parallel to scene load :currentScenes {currentScenes}");
             // It would be optimal to remove shaders and effects and lower quality options here to ensure we spend no/little time rendering
-            Scene = SceneManager.LoadSceneAsync("SimGame", LoadSceneMode.Single);
+            if (SceneManager.GetAllScenes().ToList().Exists(s => s.name == "SimGame")) { Log("Sim game exists already - unloading");
+                                                                                         Trap(() => SceneManager.UnloadScene("SimGame"));
+                                                                                         Log("Sim game unloaded"); }
+            Scene = Trap(() => SceneManager.LoadSceneAsync("SimGame", LoadSceneMode.Single));
+        }
+
+        public static IEnumerator WaitScene(AsyncOperation op) {
+            Log("[WaitScene] load scene");
+            while(!op.isDone) {
+                Log("[WaitScene] Scene not loaded. Waiting");
+                yield return null;
+            }
+            yield return null; // wait for single frame.
+            Log("[WaitScene] Done");
         }
     }
 
@@ -102,6 +137,7 @@ namespace BattletechPerformanceFix
         public void Start() {
             Log("Invocation started");
             StartCoroutine(InvokeWithComplete());
+            GameObject.DontDestroyOnLoad(this.gameObject);
         }
 
         public IEnumerator InvokeWithComplete() {
@@ -115,6 +151,10 @@ namespace BattletechPerformanceFix
             var invoker = go.AddComponent<CoroutineInvoker>();
             invoker.Enumerator = enumerator;
             invoker.complete = done;
+        }
+
+        public static void LoadSceneAsync(string scene, Action done) {
+
         }
     }
 }
