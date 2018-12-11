e5aef7a620ece8bc43ff681f69d9d1ba1f4cda3b
Matthew Spencer <m22spencer@gmail.com>
2018-12-11 08:13:01 -0600

    Start OverridePrefabCache feature

---
+++ source/OverridePrefabCache.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BattletechPerformanceFix
+{
+    class OverridePrefabCache : Feature
+    {
+        public void Activate() {
+
+
+        }
+    }
+}
