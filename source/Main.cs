57b14d990f9ed03899340d8cdaaa4b347692619b
Matthew Spencer <m22spencer@gmail.com>
2018-12-16 03:12:09 -0600

    EnableLoggingDuringLoads feature from OverridePrefabCache branch

--- source/Main.cs
+++ source/Main.cs
@@ -93,7 +93,8 @@ namespace BattletechPerformanceFix
                     { typeof(MDDB_InMemoryCache), true },
                     { typeof(ContractLagFix), true },
                     { typeof(ParallelizeLoad), false },
-                    { typeof(SimpleMetrics), false }
+                    { typeof(SimpleMetrics), false },
+                    { typeof(EnableLoggingDuringLoads), true },
                 };
                                
                 Dictionary<Type, bool> want = allFeatures.ToDictionary(f => f.Key, f => settings.features.TryGetValue(f.Key.Name, out var userBool) ? userBool : f.Value);
